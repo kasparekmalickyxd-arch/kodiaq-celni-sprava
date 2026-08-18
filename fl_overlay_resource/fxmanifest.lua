cb877178e76e7487524ed7211b2bbcec9452f4a3
kasparekmalickyxd-arch <kasparekmalickyxd@gmail.com>
2026-08-18 03:50:07 +0200

    Add FL livery overlay resource manifest

---
+++ fl_overlay_resource/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kodiaq CS FL overlay'
+description 'Celni sprava livery overlay for civkodiaqfl. Original FL vehicle remains untouched.'
+version '1.0.0'
+
+client_script 'client.lua'
